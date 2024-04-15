e486628be4d31570b452b8af36378b2bd0f3c33a
killer-cf <costa.kilder@live.com>
2024-04-15 11:21:28 -0300

    add apercu font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontFamily: {
-        sans: ['var(--font-sans)', ...fontFamily.sans],
+        sans: ['var(--font-apercu)', ...fontFamily.sans],
       },
     },
   },
